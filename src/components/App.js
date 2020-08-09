e6e8d44bc1c9cd64dff89280d463334258bbe869
Jakub Prymak <jprymak@gmail.com>
2020-08-09 20:04:27 +0200

    feat: add Preview component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import RotatingFlange from "./RotatingFlange"
-import ElementCreator from "./ElementCreator"
+import RotatingFlange from "./RotatingFlange";
+import ElementCreator from "./ElementCreator";
+import ElementList from "./ElementList";
+import Preview from "./Preview";
 
 function App() {
   return (
     <div className="App">
       <RotatingFlange/>
       <ElementCreator/>
+      <ElementList/>
+      <Preview/>
     </div>
   );
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Preview(){
+    return(
+        <section className="Preview">
+            <h2 className="Preview__header">Flanges and bolts preview</h2>
+            
+            <div className="Preview__flanges">
+                <h3>Flanges</h3>
+                <hr></hr>
+            </div>
+            <div className="Preview__bolts">
+            <h3>Bolts</h3>
+            <hr></hr>
+            </div>
+        </section>
+    )
+}
+
+export default Preview;
