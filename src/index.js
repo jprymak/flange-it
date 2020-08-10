32eaba86c9e789cd689f3cf43a413241c39343be
Jakub Prymak <jprymak@gmail.com>
2020-08-10 20:23:44 +0200

    chore: import component styles to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import "./scss/main.scss"
+import "./styles/main.scss"
+import "./styles/flange.scss";
+import "./styles/ElementCreator.scss";
+import "./styles/App.scss";
+import "./styles/ElementList.scss";
+import "./styles/Preview.scss";
 
 ReactDOM.render(
   <React.StrictMode>
