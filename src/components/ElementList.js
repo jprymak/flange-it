a5d474312bd2cc9c061b52c8dfbaf292548795ae
Jakub Prymak <jprymak@gmail.com>
2020-08-10 20:30:41 +0200

    refactor: change ElementList to class component

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,20 +1,22 @@
 import React from "react";
 
-function ElementList() {
-  return (
-    <section className="ElementList">
+class ElementList extends React.Component {
+  render() {
+    return (
+      <section className="ElementList">
         <h2>Element List</h2>
         <hr></hr>
-      <ul>
-        <li>
-          Check valve; DN50{" "}
-          <label>
-            Quantity: <input type="number" />
-          </label>{" "}
-        </li>
-      </ul>
-    </section>
-  );
+        <ul>
+          <li>
+            Check valve; DN50{" "}
+            <label>
+              Quantity: <input type="number" />
+            </label>{" "}
+          </li>
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default ElementList;
