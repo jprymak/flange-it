675cee7bb650950bc4f24673fc2086b861d465bc
Jakub Prymak <jprymak@gmail.com>
2020-08-11 20:06:55 +0200

    chore: create data.js

---
+++ src/data.js
@@ -0,0 +1,49 @@
+
+const FlangePN16 = [
+    {DN: 15, bolt: "M12", length: 50, quantity: 4},
+    {DN: 20, bolt: "M12", length: 50, quantity: 4},
+    {DN: 25, bolt: "M12", length: 50, quantity: 4},
+    {DN: 32, bolt: "M16", length: 60, quantity: 4},
+    {DN: 40, bolt: "M16", length: 60, quantity: 4},
+    {DN: 50, bolt: "M16", length: 70, quantity: 4},
+    {DN: 65, bolt: "M16", length: 70, quantity: 8},
+    {DN: 80, bolt: "M16", length: 70, quantity: 8},
+    {DN: 100, bolt: "M16", length: 70, quantity: 8},
+    {DN: 125, bolt: "M16", length: 70, quantity: 8},
+    {DN: 150, bolt: "M20", length: 70, quantity: 8},
+    {DN: 200, bolt: "M20", length: 75, quantity: 12},
+    {DN: 250, bolt: "M24", length: 85, quantity: 12},
+    ]
+    
+    const ButterflyValvePN16 = [
+      {DN: 15, bolt: "M12", length: 0, quantity: 4},
+      {DN: 20, bolt: "M12", length: 0, quantity: 4},
+      {DN: 25, bolt: "M12", length: 0, quantity: 4},
+      {DN: 32, bolt: "M16", length: 110, quantity: 4},
+      {DN: 40, bolt: "M16", length: 120, quantity: 4},
+      {DN: 50, bolt: "M16", length: 120, quantity: 4},
+      {DN: 65, bolt: "M16", length: 120, quantity: 8},
+      {DN: 80, bolt: "M16", length: 120, quantity: 8},
+      {DN: 100, bolt: "M16", length: 120, quantity: 8},
+      {DN: 125, bolt: "M16", length: 130, quantity: 8},
+      {DN: 150, bolt: "M20", length: 130, quantity: 8},
+      {DN: 200, bolt: "M20", length: 135, quantity: 12},
+      {DN: 250, bolt: "M24", length: 150, quantity: 12},
+      ]
+    
+      const CheckValvePN16 = [
+        {DN: 15, bolt: "M12", length: 0, quantity: 4},
+        {DN: 20, bolt: "M12", length: 0, quantity: 4},
+        {DN: 25, bolt: "M12", length: 0, quantity: 4},
+        {DN: 32, bolt: "M16", length: 0, quantity: 4},
+        {DN: 40, bolt: "M16", length: 75, quantity: 4},
+        {DN: 50, bolt: "M16", length: 85, quantity: 4},
+        {DN: 65, bolt: "M16", length: 85, quantity: 8},
+        {DN: 80, bolt: "M16", length: 90, quantity: 8},
+        {DN: 100, bolt: "M16", length: 90, quantity: 8},
+        {DN: 125, bolt: "M16", length: 90, quantity: 8},
+        {DN: 150, bolt: "M20", length: 90, quantity: 8},
+        {DN: 200, bolt: "M20", length: 105, quantity: 12},
+        {DN: 250, bolt: "M24", length: 120, quantity: 12},
+        ]
+        
